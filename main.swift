7667f337e0b3a393075d0a0afb03b7cf1a09274d
BravoTango7274 <aaronritch@att.net>
2020-09-04 18:30:18 +0000

    Got to hasMetStepGoal boolean

--- main.swift
+++ main.swift
@@ -1,34 +1,34 @@
 // Declare a constant called friends to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
-
-
+let friends = 57
+print(friends)
 // Now assume you go through and remove friends that aren't active on social media. Attempt to update your friends constant to a lower number than it currently is. Observe what happens and then move to the next step.
 
-
 // Does the above code compile? Why not? Print your explanation to the console using the print function. Go back and delete your line of code that updates the friends constant to a lower number so that the playground will compile properly.
-
+print("No, it is a constant and not a variable so I can't change it")
 
 // Note: These exercises reinforce Swift concepts in the context of a fitness tracking app.
 // Your fitness tracking app needs to know goal number of steps per day. Create a constant goalSteps and set it to 10000.
-
+let goalSteps = 10000
 
 // Use two print functions to print two separate lines to the console. The first line should say "Your step goal for the day is:", and the second line should print the value of goalSteps by referencing your constant.
-
-
+print("Your step goal for the day is:")
+print(goalSteps)
 // Declare a variable schooling and set it to the number of years of school that you have completed. Print schooling to the console.
-
-
+var schooling = 11
+print(schooling)
 // Now imagine you just completed an additional year of school, and update the schooling variable accordingly. Print schooling to the console.
-
-
+schooling = schooling + 1
+print(schooling)
 // Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the print function.
-
+print("Yes, it works because I changed a variable which you can change, but I was trying to change a constant previously which I can't change.")
 
 // Create a variable called steps that will keep track of the number of steps you take throughout the day. Set its initial value to 0 to represent the step count first thing in the morning. Print steps to the console.
-
-
+var steps = 0
+print(steps)
 // Now assume the tracker has been keeping track of steps all morning, and you want to show the user the latest step count. Update steps to be 2000. Print steps to the console. Then print "Good job! You're well on your way to your daily goal."
-
-
+steps = 2000
+print(steps)
+print("Good job! You're well on your way to your daily goal.")
 /* Imagine you're creating a simple photo sharing app. You want to keep track of the following metrics for each post:
 
 Number of likes: the number of likes that a photo has received
@@ -39,7 +39,16 @@ Day created: The day of the month the post was created
 
 For each of the metrics above, declare either a constant or a variable and assign it a value corresponding to a hypothetical post. Be sure to use proper naming conventions.
 */
-
+var numOfLikes = 77
+print("I chose var because the number of likes will change")
+var numOfCommets = 30
+print("I chose var because the number of comment will change")
+let yearCreated = 2015
+print("I chose let because it will always have been created that year")
+let monthCreated = "August"
+print("I chose let because it will always have been created that month")
+let dayCreated = 9
+print("I chose let because it will always have been created that day")
 
 /* There are all sorts of things that a fitness tracking app needs to keep track of in order to display the right information to the user. Similar to the last exercise, declare either a constant or a variable for each of the following items, and assign each a sensible value. Be sure to use proper naming conventions.
 
@@ -49,29 +58,37 @@ Number of steps taken today: The number of steps that a user has taken today
 Goal number of steps: The user's goal for number of steps to take each day
 Average heart rate: The user's average heart rate over the last 24 hours
 */
-
+let name = "Aaron"
+print("I chose let because Aaron will always be my name")
+var age = 16
+print("I chose var because my age will change")
+var numStepsToday = 1700
+print("I chose var because I can still add steps")
+let numStepsGoal = 2500
+print("I chose let because the goal for the number of steps will be the same")
+var averageHeartRate = 90
+print("I chose var because my heart rate could still go up or down")
 // Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
-
 
 // Type Safety
 // Declare two variables, one called firstDecimal and one called secondDecimal. Both should have decimal values. Look at both of their types by holding Option and clicking on the variable name.
-
-
+var firstDecimal = 1.5
+var secondDecimal = 5.1
 // Declare a variable called trueOrFalse and give it a boolean value. Try to assign it to firstDecimal like so: firstDecimal = trueOrFalse. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var trueOrFalse = true
+print("No because I tried to assign a bool value for a double")
 // Declare a variable and give it a string value. Then try to assign it to firstDecimal. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var test = "Hi"
+print("No because I am assigning a sting value to a double")
 // Finally, declare a variable with a whole number value. Then try to assign it to firstDecimal. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
-
+var num = 7
+print("It did not work because i was trying to assign a whole number to a double")
 // You have declared a number of constants and variables to keep track of fitness information. Declare one more variable with a boolean value called hasMetStepGoal.
-
+var hasMetStepGoal = false
 
 // When you declared a constant for goal number of steps and a variable for current step count, you likely assigned each a value in the thousands. This can be difficult to read. Redeclare this constant and variable and, when assigning each a value in the thousands, format the number so that it is more readable.
-
-
+let numStepsGoal = 25
+var numStepsToday = 17
 // Declare a variable called name of type String, but do not give it a value. Print name to the console. Does the code compile? Remove any code that will not compile.
 
 
